41ef6550e9595048497b2006c7c3462290f227ae
Gustavo Chaves Pedroza <68268899+gustavonikov@users.noreply.github.com>
2020-08-20 03:25:38 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,14 +13,9 @@ module.exports = {
     rules: {
         'linebreak-style': ['off'],
         'object-curly-newline': ['off'],
-        'react/jsx-indent': ['off'],
-        'react/jsx-indent-props': ['off'],
         indent: ['error', 4],
         'no-else-return': ['error', { allowElseIf: true }],
-        'react/jsx-one-expression-per-line': ['off'],
-        'react/no-array-index-key': ['off'],
         'no-console': ['off'],
         'no-return-assign': ['off'],
-        // "react/prop-types": ['off']
     },
 };
